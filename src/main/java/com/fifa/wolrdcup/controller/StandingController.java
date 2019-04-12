ad5f9c6959453d9ba5e953a7c26f861ad59434e8
Nirmel Murtic <nirmel.murtic@gmail.com>
2019-04-12 11:23:07 +0200

    - init standings

--- src/main/java/com/fifa/wolrdcup/controller/StandingController.java
+++ src/main/java/com/fifa/wolrdcup/controller/StandingController.java
@@ -1,8 +1,14 @@
 package com.fifa.wolrdcup.controller;
 
+import com.fifa.wolrdcup.model.Team;
 import com.fifa.wolrdcup.repository.TeamRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 @RestController
@@ -15,5 +21,20 @@ public class StandingController {
         this.teamRepository = teamRepository;
     }
 
+    @GetMapping
+    public Iterable<HashMap<String, Object>> getStandings() {
+        List<HashMap<String, Object>> result = new ArrayList<>();
 
+        Iterable<Team> teams = teamRepository.findAll();
+
+        for(Team team : teams){
+            HashMap<String, Object> teamMap = new HashMap<>();
+            teamMap.put("teamId", team.getId());
+            teamMap.put("teamName", team.getName());
+
+            result.add(teamMap);
+        }
+
+        return result;
+    }
 }
