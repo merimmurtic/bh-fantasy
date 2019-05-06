2e29d2c81d07692a1ad0113ce54a3cb8840d84c6
nirmel-murtic <nirmel.murtic@gmail.com>
2019-05-06 16:27:35 +0200

    - added CORS configuration

--- src/main/java/com/fifa/wolrdcup/WorldcupApplication.java
+++ src/main/java/com/fifa/wolrdcup/WorldcupApplication.java
@@ -11,6 +11,12 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class WorldcupApplication {
@@ -70,6 +76,18 @@ public class WorldcupApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorldcupApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
