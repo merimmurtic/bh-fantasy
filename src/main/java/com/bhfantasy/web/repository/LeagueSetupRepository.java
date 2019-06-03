89ed7f195a487f79485ec41b989a9cea7b4ce550
Nirmel Murtic <nirmel.murtic@gmail.com>
2019-06-03 23:31:40 +0200

    - added scheduler to refresh leagues

--- src/main/java/com/bhfantasy/web/repository/LeagueSetupRepository.java
+++ src/main/java/com/bhfantasy/web/repository/LeagueSetupRepository.java
@@ -1,7 +1,19 @@
 package com.bhfantasy.web.repository;
 
 import com.bhfantasy.web.model.LeagueSetup;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface LeagueSetupRepository extends CrudRepository<LeagueSetup, Long> {
+
+    @Query("select distinct leagueSetup from LeagueSetup leagueSetup " +
+            "join leagueSetup.league league " +
+            "join league.rounds round " +
+            "join round.matches match " +
+            "where leagueSetup.transfermarktUrl is not null and match.dateTime > :today and match.dateTime < :tomorrow " +
+            "order by leagueSetup.id")
+    Iterable<LeagueSetup> findLeaguesBetween(@Param("today") LocalDateTime today, @Param("tomorrow") LocalDateTime tomorrow);
 }
--- src/main/java/com/bhfantasy/web/service/LeagueSetupService.java
+++ src/main/java/com/bhfantasy/web/service/LeagueSetupService.java
@@ -3,11 +3,14 @@ package com.bhfantasy.web.service;
 import com.bhfantasy.web.model.LeagueSetup;
 import com.bhfantasy.web.model.league.RegularLeague;
 import com.bhfantasy.web.repository.LeagueSetupRepository;
+import com.bhfantasy.web.workers.TransferMarktWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -15,10 +18,15 @@ public class LeagueSetupService {
 
     private final LeagueSetupRepository leagueSetupRepository;
 
+    private final TransferMarktWorker transferMarktWorker;
+
     private static Logger logger = LoggerFactory.getLogger(LeagueSetupService.class);
 
-    public LeagueSetupService(LeagueSetupRepository leagueSetupRepository) {
+    private static boolean WORKERS_RUNNING = false;
+
+    public LeagueSetupService(LeagueSetupRepository leagueSetupRepository, TransferMarktWorker transferMarktWorker) {
         this.leagueSetupRepository = leagueSetupRepository;
+        this.transferMarktWorker = transferMarktWorker;
     }
 
     @Transactional
@@ -33,6 +41,28 @@ public class LeagueSetupService {
 
                 leagueSetupRepository.save(setup);
             }
+        }
+    }
+
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void startWorkers() throws Exception {
+        if (WORKERS_RUNNING) {
+            return;
+        }
+
+        try {
+            WORKERS_RUNNING = true;
+
+            Iterable<LeagueSetup> leagueSetups = leagueSetupRepository.findLeaguesBetween(
+                    LocalDateTime.now().minusDays(90), LocalDateTime.now().plusDays(1));
+
+            for(LeagueSetup setup : leagueSetups) {
+                if(setup.getTransfermarktUrl() != null) {
+                    transferMarktWorker.process(setup.getTransfermarktUrl());
+                }
+            }
+        } finally {
+            WORKERS_RUNNING = false;
         }
     }
 }
